b43dc0ae038c35473cc7467f046335b1e919cd91
Stan Goss <gmixlpixl@gmail.com>
2015-10-09 16:21:15 +0000

    Revert "Changed default email to Afi's to test"
    This reverts commit e90b7dbd64268661716bb80b332de0ff1254718f.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'afiimani@gmail.com'
+   default to: 'afiandstan@gmail.com'
    
    def contact_email(name, email, body)
        @name = name
