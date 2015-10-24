e3266fb6dda05b81f23193965afefdd33e845e0f
Stan Goss <gmixlpixl@gmail.com>
2015-10-24 17:59:56 +0000

    Added user controller and show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,6 +10,7 @@ class ProfilesController < ApplicationController
        @user = User.find( params[:user_id] )
        @profile = @user.build_profile(profile_params)
        if @profile.save
+           flash[:success] = "Profile Updated!"
            redirect_to user_path( params[:user_id] )
        else
            render action: :new
