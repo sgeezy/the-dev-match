6e13f0036f562d19716edf9a63a3e7b8ef4871b9
Stan Goss <gmixlpixl@gmail.com>
2016-12-09 20:50:20 +0000

    Precompile assets for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,33 +14,3 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
-/* global $*/
-//An attempt to introduce API call for bootswatch
-// $(document).ready(function(){
-//     $.getJSON("https://bootswatch.com/api/3.json",
-//         "Cerulean"
-//         );
-    // function (data) {
-    //   var themes = data.themes;
-    //   var select = "15";
-    //   select.show();
-    //   $(".alert").toggleClass("alert-info alert-success");
-    //   $(".alert h4").text("Success!");
-      
-    //   themes.forEach(function(value, index){
-    //     select.append($("<option />")
-    //           .val(index)
-    //           .text(value.name));
-    //   });
-      
-    //   select.change(function(){
-    //     var theme = themes[$(this).val()];
-    //     $("link").attr("href", theme.css);
-    //     $("h1").text(theme.name);
-    //   }).change();
-    
-    // }, "json").fail(function(){
-    //     $(".alert").toggleClass("alert-info alert-danger");
-    //     $(".alert h4").text("Failure!");
-    // }); 
-});
